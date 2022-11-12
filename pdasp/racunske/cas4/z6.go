7f9bf8ba0d018d620f5441e1c268d9e3ac05a944
Stefan <stefan.sa.aleksic@gmail.com>
2022-11-12 01:54:52 +0100

    PDASP Dining Philosophers with semaphore and two mutexes

--- pdasp/racunske/cas4/z6.go
+++ pdasp/racunske/cas4/z6.go
@@ -2,28 +2,93 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
+var t int = 0
+
+type Semaphore struct {
+	size, counter uint
+	signal        chan interface{}
+	guard         sync.Mutex
+}
+
+func (s *Semaphore) init(size uint) {
+	s.size = size
+	s.counter = 0
+	s.signal = make(chan interface{}, s.size)
+	for i := uint(0); i < s.size; i++ {
+		s.signal <- true
+	}
+}
+
+func (s *Semaphore) aquire() {
+	for {
+		select {
+		case <-s.signal:
+			s.guard.Lock()
+			s.counter++
+			s.guard.Unlock()
+			return
+		}
+	}
+}
+
+func (s *Semaphore) release() {
+	s.guard.Lock()
+	if s.counter > 0 {
+		s.counter--
+		s.guard.Unlock()
+		s.signal <- true
+	} else {
+		s.guard.Unlock()
+	}
+}
+
 type Fork struct {
-	pipe chan interface{}
+	pipe  chan interface{}
+	mutex sync.Mutex
 }
 
 type Philosopher struct {
 	name           string
 	left, right    *Fork
 	aTimes, tTimes int
+	semaphore      *Semaphore
 }
 
 func (p *Philosopher) think() {
-	fmt.Printf("%s: Thinking\n", p.name)
+	//fmt.Printf("%s: Thinking\n", p.name)
 	p.tTimes++
 	time.Sleep(time.Microsecond)
 }
 
 func (p *Philosopher) eat() {
-	fmt.Printf("%s: Eating!\n", p.name)
+	//fmt.Printf("%s: Eating!\n", p.name)
 	p.aTimes++
+}
+
+func (p *Philosopher) use() float64 {
+	return float64(p.aTimes) / float64(p.tTimes+p.aTimes)
+}
+
+func (p *Philosopher) existM(done chan interface{}) {
+	for {
+		select {
+		case <-done:
+			return
+		default:
+			p.think()
+			p.semaphore.aquire()
+			p.left.mutex.Lock()
+			p.right.mutex.Lock()
+			p.eat()
+			p.right.mutex.Unlock()
+			p.left.mutex.Unlock()
+			p.semaphore.release()
+		}
+	}
 }
 
 func (p *Philosopher) exist(done chan interface{}) {
@@ -59,33 +124,33 @@ func (p *Philosopher) exist(done chan interface{}) {
 				}
 			}()
 		case <-done:
+			fmt.Printf("%s: Done!\n", p.name)
 			return
+		default:
+			p.think()
 		}
 	}
 }
 
 func main() {
 	size := 5
-	forks := []*Fork{
-		&Fork{make(chan interface{}, 1)},
-		&Fork{make(chan interface{}, 1)},
-		&Fork{make(chan interface{}, 1)},
-		&Fork{make(chan interface{}, 1)},
-		&Fork{make(chan interface{}, 1)}}
-	philosophers := []*Philosopher{
-		&Philosopher{"Aristotle", nil, nil, 0, 0},
-		&Philosopher{"Karl Marks", nil, nil, 0, 0},
-		&Philosopher{"Immanuel Kant", nil, nil, 0, 0},
-		&Philosopher{"Plato", nil, nil, 0, 0},
-		&Philosopher{"René Descartes", nil, nil, 0, 0}}
+	semaphore := Semaphore{}
+	semaphore.init(uint(size - 1))
+
+	forks, philosophers := make([]*Fork, size), make([]*Philosopher, size)
+	names := []string{"Aristotle", "Karl Marks", "Immanuel Kant", "Plato", "René Descartes"}
+
 	for i := 0; i < size; i++ {
-		philosophers[i].left = forks[i]
-		philosophers[i].right = forks[(i+1)%size]
+		forks[i] = &Fork{make(chan interface{}, 1), sync.Mutex{}}
+		if i > 0 {
+			philosophers[i] = &Philosopher{names[i], forks[i-1], forks[i], 0, 0, &semaphore}
+		}
 	}
+	philosophers[0] = &Philosopher{names[0], forks[size-1], forks[0], 0, 0, &semaphore}
 
 	done := make(chan interface{}, 1)
 	for _, p := range philosophers {
-		go p.exist(done)
+		go p.existM(done)
 	}
 
 	for _, f := range forks {
@@ -99,6 +164,6 @@ func main() {
 	}
 
 	for _, p := range philosophers {
-		fmt.Printf("%s: ate(%d) thought(%d)\n", p.name, p.aTimes, p.tTimes)
+		fmt.Printf("%s: ate(%d) thought(%d) => %.3f\n", p.name, p.aTimes, p.tTimes, p.use())
 	}
 }
