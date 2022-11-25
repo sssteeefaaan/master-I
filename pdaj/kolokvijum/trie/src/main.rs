f5c72d6bd988c9d389a0dd0b1e004e0ac81b61f0
Stefan <stefan.sa.aleksic@gmail.com>
2022-11-25 11:57:21 +0100

    PDAJ kolokvijum

---
+++ pdaj/kolokvijum/trie/src/main.rs
@@ -0,0 +1,68 @@
+use std::collections::HashMap;
+
+
+#[derive(Debug)]
+struct Trie<T>{
+    value: Option<T>,
+    key: char,
+    children: HashMap<char, Box<Trie<T>>>
+}
+
+impl<T> Trie<T>{
+    fn new() -> Self{
+        Trie{
+            value: None,
+            key: '\0',
+            children: HashMap::new()
+        }
+    }
+
+    fn insert(&mut self, key: String, value: T)->bool{
+        if key.len() < 1{
+            return false;
+        }
+
+        let first = key.chars().nth(0).unwrap();
+        if self.key == first && key.len() == 1{
+            self.value = Some(value);
+            return true;
+        }
+
+        let (new_first, rest) =
+        if self.key == '\0'{
+            // Root
+            (first, key.clone())
+        }else{
+            (key.chars().nth(1).unwrap(), key.get(1..).unwrap().to_string())
+        };
+
+        match self.children.get_mut(&new_first){
+            Some(child)=>{
+                child.insert(rest, value)
+            },
+            None=>{
+                let mut new_child = Trie{
+                    key: new_first.clone(),
+                    value:None,
+                    children:HashMap::new()
+                };
+                let ret = new_child.insert(rest, value);
+                self.children.insert(new_first, Box::new(new_child));
+                ret
+            }
+        }
+    }
+}
+
+fn main(){
+
+    let mut t = Trie::new();
+
+    t.insert("Proba".to_string(), 15);
+    t.insert("Probam".to_string(), 10);
+    t.insert("Probali".to_string(), 11);
+    t.insert("Probacu".to_string(), 12);
+    t.insert("Probno".to_string(), 13);
+
+    println!("{:#?}", t);
+}
